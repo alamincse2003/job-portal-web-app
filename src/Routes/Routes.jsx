fdff2cc5c67d76e856044632862b9fafc8ab5b50
alamincse2003 <mdalamincse2003@gmail.com>
2025-08-23 23:39:47 +0600

    vercel.json file create

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,11 @@ import Main from "../Layout/Main";
 import { createBrowserRouter } from "react-router-dom";
 import Jobs from "../pages/Jobs";
 import JobsDetails from "../pages/JobsDetails";
-import Bookmarks from "../pages/Bookmarks";
 import AppliedJobs from "../pages/AppliedJobs";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
 import PrivateRoute from "./PrivateRoute";
+import Bookmarks from "../pages/Bookmarks";
 export const router = createBrowserRouter([
   {
     path: "/",
